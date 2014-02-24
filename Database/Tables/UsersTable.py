12ec7b75f84af7318422b09aaa7d842dbde3f4ef
Andreas Papageorgiou <andreasppg92@gmail.com>
2014-02-24 21:29:25 +0000

    Start working

--- Database/Tables/UsersTable.py
+++ Database/Tables/UsersTable.py
@@ -7,3 +7,19 @@ class UsersTable(BaseTable):
       "username": "text PRIMARY KEY",
       "password": "text"
     }
+
+  def addUser(self, username, password):
+    command = "INSERT INTO " + self.name + "VALUES (%s, %s)"
+    self.cursor.execute(command, (username, password))
+    self.cursor.connection.commit()
+
+  def userExists(self, username):
+    command = "SELECT * FROM " + self.name + " WHERE username=%s"
+    self.cursor.execute(command, (username,))
+    return self.cursor.rowcount != 0
+
+  def checkLogin(self, username, password):
+    command = "SELECT * FROM " + self.name + \
+              " WHERE username=%s AND password=%s"
+    self.cursor.execute(command)
+    return self.cursor.rowcount == 1
